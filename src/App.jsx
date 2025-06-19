a4eed574b9a652a162e7cee0145c9300a4529882
ParisAngelopoulos <156656889+ParisAngelopoulos@users.noreply.github.com>
2025-06-19 15:26:17 +0200

    Add data fetching logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch('http://leenplaats.test/api/testdata')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error fetching data:', error));
+  });
 
   return (
     <>
